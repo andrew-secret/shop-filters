12533688bb21c3d94677ea71634f10ee3b52f6ca
andrew-secret <andre.rusakow@jimdo.com>
2017-12-09 21:31:35 +0100

    feat: add helper functions

---
+++ src/lib/shopHelper.js
@@ -0,0 +1,22 @@
+export const findById = (id, list) => list.find(item => item.id === id);
+
+export const toggleClass = (listItem, list) => {
+
+    return(
+        {
+            ...listItem,
+            isActive: !listItem.isActive,
+        }
+    );
+};
+
+export const updateList = (list, updated) => {
+    const updatedIndex = list.findIndex(item => item.id === updated.id);
+    return [
+      ...list.slice(0, updatedIndex),
+      updated,
+      ...list.slice(updatedIndex+1)
+    ]
+};
+
+export const partial = (fn, ...args) => fn.bind(null, ...args);
