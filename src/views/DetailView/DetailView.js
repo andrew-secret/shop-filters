4d665b2571de1edc66b563423f35ff386924e107
andrew-secret <andre.rusakow@jimdo.com>
2018-02-01 21:19:12 +0100

    feat: lift state down to detailView and add conditional rendering

--- src/views/DetailView/DetailView.js
+++ src/views/DetailView/DetailView.js
@@ -1,61 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '../../components/Button/Button';
 import { findById } from '../../lib/scripts/shopHelper';
 import { formatPrice } from '../../lib/scripts/shopHelper';
 import SelectList from '../../components/SelectList/SelectList';
 import { CSSTransitionGroup } from 'react-transition-group';
+import { base } from '../../base';
 import './DetailView.css';
 
+const defaultValue = 'Select your size:';
 const selectList = [
-    {id: 1, value: 'S'},
     {id: 2, value: 'M'},
     {id: 3, value: 'L'},
     {id: 4, value: 'XL'}
 ];
 
-const defaultValue = 'Select your size:';
+class DetailView extends Component {
+    componentWillMount () {
+        this.shopItemsRef = base.syncState('shopitems', {
+            context: this,
+            state: 'shopitems'
+        });
+    }
 
-const DetailView = props => {
-    const id = parseInt(props.match.params.id, 10);
-    const shopItem = findById(id, props.shopitems);
-    const price = formatPrice(shopItem.price);
-    const addToCart = 'Add to cart';
+    componentWillUnmount() {
+        base.removeBinding(this.shopItemsRef);
+    }
 
-    return (
-        <CSSTransitionGroup
-            transitionName="fade-in"
-            transitionAppear={true}
-            transitionAppearTimeout={500}
-            transitionEnterTimeout={500}
-            transitionLeaveTimeout={500}>
-            <div className="detailView">
-                <div className="detailView__left">
-                    <img src={require(`../../assets/shopitems/${shopItem.image}`)}
-                        className="shopItem__image"
-                        alt={shopItem.model}/>
+    render() {
+        const id = parseInt(this.props.match.params.id, 10);
+        const shopItem = findById(id, this.props.shopitems);
+        const addToCart = 'Add to cart';
+        return (
+            <div>
+                <CSSTransitionGroup
+                    transitionName="fade-in"
+                    transitionAppear={true}
+                    transitionAppearTimeout={500}
+                    transitionEnterTimeout={500}
+                    transitionLeaveTimeout={500}>
+                {shopItem !== undefined ? (
+                    <div className="detailView">
+                    <div className="detailView__left">
+                        <img src={require(`../../assets/shopitems/${shopItem.image}`)}
+                            className="shopItem__image"
+                            alt={shopItem.model}/>
+                    </div>
+                    <div className="detailView__right">
+                        <span className="detailView__brand">
+                            {shopItem.brand}
+                        </span>
+                        <h1 className="detailView__h1">
+                            {shopItem.model}
+                        </h1>
+                        <span className="detailView__price">
+                            {shopItem.price}
+                        </span>
+                        <SelectList
+                            className="detailView__select"
+                            defaultValue={defaultValue}
+                            selectList={selectList}/>
+                        <Button
+                            label={addToCart}
+                            className="detailView__button"
+                        />
+                    </div>
                 </div>
-                <div className="detailView__right">
-                    <span className="detailView__brand">
-                        {shopItem.brand}
-                    </span>
-                    <h1 className="detailView__h1">
-                        {shopItem.model}
-                    </h1>
-                    <span className="detailView__price">
-                        {price}
-                    </span>
-                    <SelectList
-                        className="detailView__select"
-                        defaultValue={defaultValue}
-                        selectList={selectList}/>
-                    <Button
-                        label={addToCart}
-                        className="detailView__button"
-                    />
-                </div>
+                ) : null}
+                </CSSTransitionGroup>
             </div>
-        </CSSTransitionGroup>
-    );
+        );
+    }
 }
 
 export default DetailView;
